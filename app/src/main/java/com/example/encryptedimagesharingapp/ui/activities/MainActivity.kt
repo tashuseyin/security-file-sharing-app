2061b6be6b706fb0c2daeb312c440087849050e5
Monster <htas767@gmail.com>
2021-11-01 16:16:32 +0300

    init

--- app/src/main/java/com/example/encryptedimagesharingapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/encryptedimagesharingapp/ui/activities/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.encryptedimagesharingapp.ui.activities
 
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.example.encryptedimagesharingapp.R
 import com.example.encryptedimagesharingapp.databinding.ActivityMainBinding
-import kotlinx.coroutines.delay
+import github.com.st235.lib_expandablebottombar.ExpandableBottomBar
+import github.com.st235.lib_expandablebottombar.MenuItemDescriptor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,5 +17,47 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_EncryptedImageSharingApp)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomMenu()
+
+    }
+
+
+    @SuppressLint("ResourceAsColor")
+    private fun bottomMenu() {
+        val bottomBar: ExpandableBottomBar = binding.expandableBottomBar
+        val menu = bottomBar.menu
+        menu.add(
+            MenuItemDescriptor.Builder(
+                this,
+                R.id.home,
+                R.drawable.ic_baseline_home_24,
+                R.string.home,
+                Color.BLUE
+            )
+                .build()
+        )
+        menu.add(
+            MenuItemDescriptor.Builder(
+                this,
+                R.id.users,
+                R.drawable.ic_baseline_person_24,
+                R.string.users,
+                Color.BLUE
+            )
+                .build()
+        )
+        menu.add(
+            MenuItemDescriptor.Builder(
+                this,
+                R.id.settings,
+                R.drawable.ic_baseline_settings_24,
+                R.string.settings,
+                Color.BLUE
+            )
+                .build()
+        )
+
     }
 }
+
