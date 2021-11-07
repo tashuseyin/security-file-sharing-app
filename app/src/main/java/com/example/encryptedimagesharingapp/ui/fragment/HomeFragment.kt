3d77156ef574237b3b910b54490ee766a125e17c
Monster <htas767@gmail.com>
2021-11-07 17:04:38 +0300

    file remove

--- app/src/main/java/com/example/encryptedimagesharingapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/encryptedimagesharingapp/ui/fragment/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.example.encryptedimagesharingapp.R
 import com.example.encryptedimagesharingapp.databinding.FragmentHomeBinding
 import com.example.encryptedimagesharingapp.ui.activities.MainActivity
@@ -17,8 +16,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.io.File
 
 
